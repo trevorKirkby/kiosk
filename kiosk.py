8fdec05e7342b50129364ae10a036c586c14ee83
Trevor Kirkby <trevor.p.kirkby@gmail.com>
2019-08-19 14:19:55 -0700

    Selenium functionality implemented.

--- kiosk.py
+++ kiosk.py
@@ -1,5 +1,13 @@
 import socket
 from flask import Flask, request, redirect, render_template
+from selenium import webdriver
+
+PORT = 4321
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--disable-infobars")
+
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options = chrome_options)
 
 app = Flask(__name__, static_url_path="")
 app.url_map.strict_slashes = False
@@ -11,6 +19,7 @@ def config():
     global destination
     if request.method == "POST":
         destination = request.form["URL"]
+        driver.get(destination)
     return render_template("config.html", destination=destination)
 
 @app.route("/display")
@@ -18,5 +27,10 @@ def display():
     return redirect(destination)
 
 if __name__ == "__main__":
-    host_ip = ([(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
-    app.run(host=host_ip, port=4321)
+    try:
+        host_ip = ([(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+        driver.get(destination)
+        driver.fullscreen_window()
+        app.run(host=host_ip, port=PORT)
+    finally:
+        driver.quit()
