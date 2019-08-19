c1dc7607b0006af8dd1e2ca909e9c8405c0c4a4f
Trevor Kirkby <trevor.p.kirkby@gmail.com>
2019-08-19 16:08:11 -0700

    user friendliness

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # kiosk
 simple web-server for displaying a web page from a raspberry pi
+
+# Setup
+0. If installing Raspbian, install Raspbian Desktop with recommended software.
+1. Install the latest versions of Chromium Browser and the Unclutter package: `sudo apt-get install chromium-browser unclutter`
+2. Set up Selenium for use with chromium within python according to [this guide](https://www.reddit.com/r/selenium/comments/7341wt/success_how_to_run_selenium_chrome_webdriver_on/).
+3. Install the flask library for python: `pip install flask`
+4. Add the following lines to make kiosk and unclutter run on startup: `sudo nano /etc/profile`
+```
+unclutter &
+python /home/pi/kiosk/kiosk.py &
+```
+
+# Usage
+Reboot to start kiosk mode. Visit the page `[IP address]:[port]/config` to change what URL is being displayed.
+Kiosk will initially display the correct IP address after booting.
+Will run on port 4321 by default. Port may be configured as a constant in kiosk.py.
--- kiosk.py
+++ kiosk.py
@@ -1,18 +1,21 @@
 import socket
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 PORT = 4321
+host_ip = ([(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--disable-infobars")
 
 driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options = chrome_options)
+action = ActionChains(driver)
 
 app = Flask(__name__, static_url_path="")
 app.url_map.strict_slashes = False
 
-destination = "https://www.google.com/"
+destination = "http://www.google.com"
 
 @app.route("/config", methods=['GET', 'POST'])
 def config():
@@ -28,9 +31,10 @@ def display():
 
 if __name__ == "__main__":
     try:
-        host_ip = ([(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
         driver.get(destination)
         driver.fullscreen_window()
+        search_box = driver.find_element_by_name('q')
+        search_box.send_keys(host_ip)
         app.run(host=host_ip, port=PORT)
     finally:
         driver.quit()
